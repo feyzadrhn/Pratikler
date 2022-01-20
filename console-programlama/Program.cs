ab0756721b136fc521794f02876f9a1c12469d27
Feyza Durhan <60888945+feyzadrhn@users.noreply.github.com>
2022-01-20 16:59:20 +0300

    console-programlama

---
+++ console-programlama/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace console_programlama
+{
+    class Program 
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Adınızı Giriniz");
+            string name= Console.ReadLine();
+            Console.WriteLine("Soyadınızı Giriniz");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Merhaba "+ name + " " + surname);
+
+
+        }
+    }
+}
+
